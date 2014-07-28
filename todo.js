f50351590e2838b5b0b0b24594ebe152018bad5a
Superman2971 <goldfarb.ian@gmail.com>
2014-07-28 15:23:23 -0700

    Worked on it to configure as object for firebase...still not fixed (but is functional)

--- todo.js
+++ todo.js
@@ -3,13 +3,15 @@ var ToDoList_App = angular.module("ToDoList", ["ui.bootstrap", "firebase"]);
 ToDoList_App.controller("ToDoList_Controller", function($scope, $firebase){
 
   // THE LIST OF ITEMS
-  $scope.list = [];
+  $scope.list = {
+    list:[]
+  };
 
   // ADD AN ITEM FUNCTION
   $scope.adding = function(key, value){
     if (key.keyCode == 13){
       if (value){
-        $scope.list.splice(0, 0, {done: false, text: value});
+        $scope.list.list.splice(0, 0, {done: false, text: value});
         $scope.Text = null;
       }
     }
@@ -17,26 +19,21 @@ ToDoList_App.controller("ToDoList_Controller", function($scope, $firebase){
 
   // REMOVE COMPLETED ITEMS FUNCTION
   $scope.removing = function(){
-    var x = $scope.list.length;
-    $scope.list2 = []
+    var x = $scope.list.list.length;
+    $scope.list2 = [];
     for (var i = 0; i < x; i++){
-      if ($scope.list[i].done == false){
-        $scope.list2.push($scope.list[i]);
+      if ($scope.list.list[i].done == false){
+        $scope.list2.push($scope.list.list[i]);
       }
-    } $scope.list = $scope.list2;
-  };
-
-  // CHANGE ARRAY TO OBJECT FOR FIREBASE
-  $scope.object_list = {
-    list_object: $scope.list
+    } $scope.list.list = $scope.list2;
   };
 
   // FIREBASE
   var Fire_items = new Firebase("https://to-do-list-practice.firebaseio.com/" + "ToDo_items");
   $scope.remote_list = $firebase(Fire_items);
 
-  $scope.remote_list.$bind($scope, "object_list");
-  $scope.$watch("object_list", function(){
+  $scope.remote_list.$bind($scope, "list");
+  $scope.$watch("list", function(){
     return false;
   });
 
